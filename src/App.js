9a08420dcb5d3ca4eeb42fcdc5905f330b5b5af4
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-05-19 00:12:41 +0300

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
 
+const API_URL = 'http://www.omdbapi.com?apikey=33af8d35';
+
 function App() {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  }
+  useEffect(() => {
+    searchMovies('Spiderman')
+  }, []);
   return (
   <h1>App</h1>
   );
